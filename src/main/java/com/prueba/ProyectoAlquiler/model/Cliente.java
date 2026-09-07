22b9a68d612bbf8c437ca1e73d25cc21065d0d7b
karxerOa <ortizacevedodulcemaria@gmail.com>
2026-09-06 21:42:20 -0500

    Se agregaron atributos a Cliente y Empleado

--- src/main/java/com/prueba/ProyectoAlquiler/model/Cliente.java
+++ src/main/java/com/prueba/ProyectoAlquiler/model/Cliente.java
@@ -1,4 +1,21 @@
 package com.prueba.ProyectoAlquiler.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
 public class Cliente {
+    private Integer idCliente;
+    private String nombre;
+    private String apellidos;
+    private int dni;
+    private int telefono;
+    private String direccion;
+    private String correo;
 }
