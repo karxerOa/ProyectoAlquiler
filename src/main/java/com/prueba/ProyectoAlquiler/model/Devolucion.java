6f012bd74a67be6c8327a7fd03d476c66a51a273
Abner2025l <abnervazquezchava123@gmail.com>
2026-09-07 13:37:03 -0500

    Se agregaron atributos a las entidaddes Devolucion y  Penalidad

--- src/main/java/com/prueba/ProyectoAlquiler/model/Devolucion.java
+++ src/main/java/com/prueba/ProyectoAlquiler/model/Devolucion.java
@@ -1,4 +1,32 @@
 package com.prueba.ProyectoAlquiler.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Devolucion {
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer idDevolucion;
+    @Column(nullable = false, unique = true,  length = 50)
+    private Date fechaDevolucion;
+    @Column(nullable = false, unique = true,  length = 50)
+    private String estadoprenda;
+    @Column(nullable = false)
+    private String observacion;
+
+    //relaciones
+    @ManyToOne
+    @JoinColumn(name="idempledado", nullable = false,foreignKey = @ForeignKey(name="FK_empleado_devolucion"))
+    private  Empleado empleado;
 }
