f245065c30b795383c0199e6ce7ef9a8309269f2
JhonPao <jhonpaolo7788@gmail.com>
2026-09-07 23:37:20 -0500

    m

---
+++ d
@@ -0,0 +1,12 @@
+  RamaJhonPao[m
+  a-[m
+* [32mmain[m
+  [31mremotes/origin/HEAD[m -> origin/main
+  [31mremotes/origin/RamaDaniel-CategoriaPorduccion[m
+  [31mremotes/origin/RamaJhonPao[m
+  [31mremotes/origin/develop[m
+  [31mremotes/origin/feat-Clases_Mantenimiento_Prenda-JHONPAOLO[m
+  [31mremotes/origin/feature/Alquiler_DetalleAlquiler-[m
+  [31mremotes/origin/feature/Entidad-Cliente-Empleado[m
+  [31mremotes/origin/feature/Entidad-DevolucionPenalidad[m
+  [31mremotes/origin/main[m
--- src/main/java/com/prueba/ProyectoAlquiler/model/Mantenimiento.java
+++ src/main/java/com/prueba/ProyectoAlquiler/model/Mantenimiento.java
@@ -1,105 +1,49 @@
 package com.prueba.ProyectoAlquiler.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Mantenimiento {
-    private int id_mantenimiento;
-    private LocalDate fecha_inicio;
-    private LocalDate fecha_fin;
-    private String tipo_mantenimiento;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "id_mantenimiento", nullable = false)
+    private Integer idMantenimiento;
+
+    @Column(name = "fecha_inicio", nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(name = "fecha_fin")
+    private LocalDate fechaFin;
+
+    @Column(name = "tipo_mantenimiento", nullable = false, length = 80)
+    private String tipoMantenimiento;
+
+    @Column(name = "descripcion", columnDefinition = "TEXT")
     private String descripcion;
+
+    @Column(name = "costo", nullable = false, precision = 10, scale = 2)
     private BigDecimal costo;
+
+    @Column(name = "estado", nullable = false, length = 20)
     private String estado;
-    private int id_prenda;
-    private int id_trabajador;
 
-    public Mantenimiento() {
-    }
+    @ManyToOne
+    @JoinColumn(name = "id_prenda", nullable = false, foreignKey = @ForeignKey(name = "FK_prenda_mantenimiento"))
+    private Prenda prenda;
 
-    public Mantenimiento(int id_mantenimiento, LocalDate fecha_inicio, LocalDate fecha_fin,
-                                String tipo_mantenimiento, String descripcion, BigDecimal costo,
-                                                String estado, int id_prenda, int id_trabajador) {
-        this.id_mantenimiento = id_mantenimiento;
-        this.fecha_inicio = fecha_inicio;
-        this.fecha_fin = fecha_fin;
-        this.tipo_mantenimiento = tipo_mantenimiento;
-        this.descripcion = descripcion;
-        this.costo = costo;
-        this.estado = estado;
-        this.id_prenda = id_prenda;
-        this.id_trabajador = id_trabajador;
-    }
-
-    public int getId_mantenimiento() {
-        return id_mantenimiento;
-    }
-
-    public void setId_mantenimiento(int id_mantenimiento) {
-        this.id_mantenimiento = id_mantenimiento;
-    }
-
-    public LocalDate getFecha_inicio() {
-        return fecha_inicio;
-    }
-
-    public void setFecha_inicio(LocalDate fecha_inicio) {
-        this.fecha_inicio = fecha_inicio;
-    }
-
-    public LocalDate getFecha_fin() {
-        return fecha_fin;
-    }
-
-    public void setFecha_fin(LocalDate fecha_fin) {
-        this.fecha_fin = fecha_fin;
-    }
-
-    public String getTipo_mantenimiento() {
-        return tipo_mantenimiento;
-    }
-
-    public void setTipo_mantenimiento(String tipo_mantenimiento) {
-        this.tipo_mantenimiento = tipo_mantenimiento;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public BigDecimal getCosto() {
-        return costo;
-    }
-
-    public void setCosto(BigDecimal costo) {
-        this.costo = costo;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public int getId_prenda() {
-        return id_prenda;
-    }
-
-    public void setId_prenda(int id_prenda) {
-        this.id_prenda = id_prenda;
-    }
-
-    public int getId_trabajador() {
-        return id_trabajador;
-    }
-
-    public void setId_trabajador(int id_trabajador) {
-        this.id_trabajador = id_trabajador;
-    }
+    @ManyToOne
+    @JoinColumn(name = "id_trabajador", nullable = false, foreignKey = @ForeignKey(name = "FK_trabajador_mantenimiento"))
+    private Empleado empleado;
 }
