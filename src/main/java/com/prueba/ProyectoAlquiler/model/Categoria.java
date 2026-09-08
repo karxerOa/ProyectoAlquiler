b8563995445cd04fb87e16c617cec01880f1aa80
FHAB\Administrator <Daniel07-cmd>
2026-09-07 20:08:28 -0500

    Creacion de las tablas _ 1.Categoria 2.Produccion

--- src/main/java/com/prueba/ProyectoAlquiler/model/Categoria.java
+++ src/main/java/com/prueba/ProyectoAlquiler/model/Categoria.java
@@ -1,4 +1,30 @@
 package com.prueba.ProyectoAlquiler.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Categoria {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private Integer idCategoria;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(columnDefinition = "TEXT")
+    private String descripcion;
+
+    @Column(nullable = false, length = 20)
+    private String estado;
 }
