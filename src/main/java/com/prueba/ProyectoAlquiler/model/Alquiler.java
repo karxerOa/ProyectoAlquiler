989793f2ec0dd79da18244a7d1902fb373412108
Mauricio1124 <karodama0411292420@gmail.com>
2026-09-07 21:36:29 -0500

    feat: creacion de tablas Alquiler y DetalleAlquiler

--- src/main/java/com/prueba/ProyectoAlquiler/model/Alquiler.java
+++ src/main/java/com/prueba/ProyectoAlquiler/model/Alquiler.java
@@ -1,4 +1,61 @@
 package com.prueba.ProyectoAlquiler.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "alquiler")
 public class Alquiler {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idAlquiler;
+
+    @Column(nullable = false)
+    private LocalDate fechaAlquiler;
+
+    @Column(nullable = false)
+    private LocalDate fechaInicio;
+
+    @Column(nullable = false)
+    private LocalDate fechaFin;
+
+    @Column(nullable = false, length = 20)
+    private String estado;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal subtotal;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal total;
+
+
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", nullable = false)
+    private Cliente cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "id_empleado", nullable = false)
+    private Empleado empleado;
+
+    @OneToMany(mappedBy = "alquiler", cascade = CascadeType.ALL)
+    private List<DetalleAlquiler> detalles;
+
+    @OneToOne(mappedBy = "alquiler", cascade = CascadeType.ALL)
+    private Pago pago;
+
+    @OneToOne(mappedBy = "alquiler", cascade = CascadeType.ALL)
+    private Devolucion devolucion;
 }
--- src/main/java/com/prueba/ProyectoAlquiler/model/DetalleAlquiler.java
+++ src/main/java/com/prueba/ProyectoAlquiler/model/DetalleAlquiler.java
@@ -1,4 +1,40 @@
 package com.prueba.ProyectoAlquiler.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "detalle_alquiler")
 public class DetalleAlquiler {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer idDetalle;
+
+    @Column(nullable = false)
+    private int cantidad;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal subtotal;
+
+    @ManyToOne
+    @JoinColumn(name = "id_alquiler", nullable = false)
+    private Alquiler alquiler;
+
+    @ManyToOne
+    @JoinColumn(name = "id_prenda", nullable = false)
+    private Prenda prenda;
 }
