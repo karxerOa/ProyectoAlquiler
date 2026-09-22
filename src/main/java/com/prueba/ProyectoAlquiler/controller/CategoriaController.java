41cae9c606e9b604b216d3d2ca99f0d076dd1053
Daniel07-cmd <n00381484@upn.pe>
2026-09-22 15:37:35 -0500

    implementacion de DTOs y ModelMapper para Categoria y Produccion
    Co-authored-by: FHAB\Administrator <Daniel07-cmd>

--- src/main/java/com/prueba/ProyectoAlquiler/controller/CategoriaController.java
+++ src/main/java/com/prueba/ProyectoAlquiler/controller/CategoriaController.java
@@ -3,29 +3,24 @@ package com.prueba.ProyectoAlquiler.controller;
 import com.prueba.ProyectoAlquiler.dto.CategoriaDto;
 import com.prueba.ProyectoAlquiler.model.Categoria;
 import com.prueba.ProyectoAlquiler.service.interfaz.ICategoriaService;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/categorias")
 public class CategoriaController {
+
     private final ICategoriaService service;
     private final ModelMapper modelMapper;
 
     public CategoriaController(ICategoriaService service,
-        @Qualifier("categoriaMapper") ModelMapper modelMapper) {
+                               @Qualifier("categoriaMapper") ModelMapper modelMapper) {
         this.service = service;
         this.modelMapper = modelMapper;
     }
@@ -33,8 +28,8 @@ public class CategoriaController {
     @GetMapping
     public ResponseEntity<List<CategoriaDto>> findAll() throws Exception {
         return ResponseEntity.ok(service.findAll().stream()
-            .map(entity -> modelMapper.map(entity, CategoriaDto.class))
-            .toList());
+                .map(entity -> modelMapper.map(entity, CategoriaDto.class))
+                .toList());
     }
 
     @GetMapping("/{id}")
@@ -45,14 +40,16 @@ public class CategoriaController {
     @PostMapping
     public ResponseEntity<CategoriaDto> save(@RequestBody CategoriaDto categoria) throws Exception {
         Categoria entity = modelMapper.map(categoria, Categoria.class);
-        return new ResponseEntity<>(modelMapper.map(service.save(entity), CategoriaDto.class), HttpStatus.CREATED);
+        Categoria obj = service.save(entity);
+        return new ResponseEntity<>(modelMapper.map(obj, CategoriaDto.class), HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<CategoriaDto> update(@PathVariable Integer id, @RequestBody CategoriaDto categoria) throws Exception {
         categoria.setIdCategoria(id);
         Categoria entity = modelMapper.map(categoria, Categoria.class);
-        return ResponseEntity.ok(modelMapper.map(service.update(id, entity), CategoriaDto.class));
+        Categoria obj = service.update(id, entity);
+        return ResponseEntity.ok(modelMapper.map(obj, CategoriaDto.class));
     }
 
     @DeleteMapping("/{id}")
